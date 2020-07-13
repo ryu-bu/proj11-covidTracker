575ae57bdaf20141f17ca484ae76823d1df4f150
Ryuichi Ohhata <ryu74@bu.edu>
2020-07-12 23:47:24 -0400

    build new apis

---
+++ backend/covid/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Town, Councilor
+
+# Register your models here.
+
+class TownAdmin(admin.ModelAdmin):
+    list_display = ('name', 'zip_code', 'positive_cases', 'tested', 'updated_date', 'councilor')
+
+admin.site.register(Town, TownAdmin)
+
+class CouncilorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'city')
+
+admin.site.register(Councilor, CouncilorAdmin)
